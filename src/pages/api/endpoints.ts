ea9694fa207648a47efe34a709aeafdfd92e0b02
Isacali <isabelcaliman010@gmail.com>
2022-08-15 14:25:34 -0300

    oi

---
+++ src/pages/api/endpoints.ts
@@ -0,0 +1,25 @@
+import { NextApiResponse } from "next";
+import { api } from "./config";
+export async function getPokemon(pokemon: string, setValue: any) {
+  api.get(`pokemon/${pokemon}`).then((res) =>
+    setValue({
+      name: res?.data.name,
+      img: res?.data.sprites?.front_default,
+    }),
+  );
+}
+
+export async function getPokemonInterval(
+  offset: number,
+  limit: number,
+  setValue: any,
+) {
+  api
+    .get("pokemon/", {
+      params: {
+        offset,
+        limit,
+      },
+    })
+    .then((res: any) => setValue(res?.data.results));
+}
